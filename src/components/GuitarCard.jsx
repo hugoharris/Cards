51aff7ac7d813d35eaa76f9d29cb089cd3c8db32
Kevin Harris <hugokharris@gmail.com>
2023-03-01 16:36:44 -0700

    make the card titles red

--- src/components/GuitarCard.jsx
+++ src/components/GuitarCard.jsx
@@ -1,13 +1,18 @@
 import React from 'react';
 import './GuitarCard.css';
 
-export default function (props) {
+export default function GuitarCard(props) {
+  const cardStyle = {
+    color: "red",
+    fontSize: "48px"
+  };
+  
   return (
     <div className="guitar-card" onClick={() => props.handleToggle(props.guitar.id)}>
-        <h2>{props.guitar.make}</h2>
+        <h2 style={cardStyle}>{props.guitar.make}</h2>
         <h3>{props.guitar.model}</h3>
         <p>{props.guitar.year}</p>
         <p className="in-stock">In stock: {props.guitar.available ? "Yes" : "No"}</p>
     </div>
-  )
+  );
 }
